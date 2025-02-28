e77fc3127727d7ccf691e2780fcd5d844b8fd855
Mysterwll <22051236@hdu.edu.cn>
2025-02-28 13:02:41 +0800

    some updates

--- run_stage2.py
+++ run_stage2.py
@@ -1,135 +1,297 @@
-import argparse
-import time
-from datetime import datetime
-from pathlib import Path
-from sklearn.model_selection import KFold
-
-import torch.utils.data
-from torch.utils.data import random_split
-
-from utils.model_object import models
-from data.dataset import Liver_dataset, Liver_normalization_dataset
-from utils.observer import Runtime_Observer
+import os
+import random
+import sys
 import numpy as np
-from data.graphdata import *
-from torch_geometric.data import Data
+import pandas as pd
+from sklearn.model_selection import KFold
+import torch
+import torch.nn as nn
+import torch.nn.functional as F
+import time
+from sklearn.discriminant_analysis import LinearDiscriminantAnalysis as LDA
+from torch_geometric.nn import TransformerConv, GraphConv, GCN2Conv, GCNConv, DenseGraphConv
+from torch.utils.data import random_split,Dataset
+from torch.nn import CrossEntropyLoss
+from torch.optim import Adam
+from tqdm import tqdm
+from observer import Runtime_Observer
 
-def prepare_to_train(model_index, seed, device, fold, data_parallel):
-    global experiment_settings
-    assert torch.cuda.is_available(), "Please ensure codes are executed on cuda."
-    try:
-        experiment_settings = models[model_index]
-    except KeyError:
-        print('model not in model_object!')
-    torch.cuda.empty_cache()
+class Liver_dataset(torch.utils.data.Dataset):
+    def __init__(self, summery_path: str = 'data/summery_new.txt', feature_path: str = 'data/vision_features', fold = 0, mode: str = 'vis'):
+        print("Dataset init ...")
+        self.data_dict = {}  # all details of data
+        self.data_list = []  # all uids of data
+        self.data = []  # all value of data
+        self.mode = mode
+        summery = open(summery_path, 'r')
+        titles = summery.readline().split()
+        titles = [title.replace('_', ' ') for title in titles]
+        df = pd.read_csv(f'data/vision_features_666/vision_features_{fold}.csv', header = 0, dtype={'uid': str})
+        # 创建一个空字典来存储标签和对应的特征列表
+        label_features = {}
 
-    '''
-    Dataset init, You can refer to the dataset format defined in data/dataset.py to define your private dataset
-    '''
-    dataset = Liver_normalization_dataset(experiment_settings['Data'], mode=experiment_settings['Dataset_mode'])
-    patient_info = get_pinfo(cli_file ="./data/summery_new.xlsx", vision_file = "./data/vision_seed42/vision_features_{}.csv".format(fold))
+        # 遍历DataFrame中的每一行
+        for index, row in df.iterrows():
+            label = str(row[0])  # 第一列是标签
+            features = row[1:].tolist()  # 剩余列是特征
+            label_features[label] = features
+        
+        count = 0
+        for item in summery:
+            count += 1
+            single_data_list = item.split()
+            temp_dict = {}
+            temp = {}
+            for i in range(len(single_data_list)):
+                temp.update({titles[i]: single_data_list[i]})
+            uid = temp.pop('uid')
+            srcid = temp.pop('srcid')
+            label = temp.pop('Outcome')
+            _data = [float(x) for x in list(temp.values())]
+            temp_dict.update({'srcid': srcid})
+            temp_dict.update({'label': label})
+
+            temp_dict.update({'features': _data})
+            temp_dict.update({'vision_feature': label_features[uid]})
+            # temp_dict.update({'source': temp})
+
+            self.data_dict.update({uid: temp_dict})
+            self.data_list.append(uid)
+            self.data.append(_data)
+        # Normalization
+
+        print("Summery loaded --> Feature_num : %d  Data_num : %d" % (len(titles) - 3, count))
+        summery.close()
+
+    def __getitem__(self, index):
+        uid = self.data_list[index]
+
+        srcid = self.data_dict[uid]['srcid']
+        text_feature = self.data_dict[uid]['features']
+        
+        vision_feature = self.data_dict[uid]['vision_feature']
+        vision_feature = torch.Tensor(vision_feature)
+        
+
+        # usage of text data
+        clinical = self.data_dict[uid]['features'][:-1781]
+        clinical_tensor = torch.Tensor(clinical)
+        clinical_tensor = torch.where(torch.isnan(clinical_tensor), torch.full_like(clinical_tensor, 0),
+                                      clinical_tensor)
+        # details
+        basic_info_tensor = torch.Tensor(clinical[:21])
+        blood_test_tensor = torch.Tensor(clinical[21:27])
+        biochemical_tensor = torch.Tensor(clinical[27:44])
+        fat_tensor = torch.Tensor(clinical[44:])
+        # print(f"basic_info_tensor : {len(basic_info_tensor)}")
+        # print(f"blood_test_tensor : {len(blood_test_tensor)}")
+        # print(f"biochemical_tensor : {len(biochemical_tensor)}")
+        # print(f"fat_tensor : {len(fat_tensor)}")
+        # basic_info_tensor : 21
+        # blood_test_tensor : 6
+        # biochemical_tensor : 17
+        # fat_tensor : 14
 
 
-    # dataset = Liver_dataset(summery_path=experiment_settings['Data'], mode=experiment_settings['Dataset_mode'])
-    torch.manual_seed(seed)
+        radio = self.data_dict[uid]['features'][-1781:]
+        radio_tensor = torch.Tensor(radio)
+        radio_tensor = torch.where(torch.isnan(radio_tensor), torch.full_like(radio_tensor, 0), radio_tensor)
 
-    '''
-    The seed in Kfold should be same!
-    '''
-    kf = KFold(n_splits=5, shuffle=True, random_state=seed)
-    train_index, test_index = [[t1, t2] for t1, t2 in kf.split(dataset)][fold]
+        label = int(self.data_dict[uid]['label'])
+        label_tensor = torch.from_numpy(np.array(label)).long()
+        
+        if self.mode == 'vis':
+            return clinical_tensor, vision_feature, label_tensor
+        elif self.mode == 'base':
+            return clinical_tensor, label_tensor
+        else:
+            return clinical_tensor, radio_tensor, label_tensor
 
-    train_uids = [dataset[i] for i in train_index]
-    test_uids = [dataset[i] for i in test_index]
+    def __len__(self):
+        return len(self.data_list)
+    
 
-    train_indices = np.where(np.isin(patient_info['uid'], train_uids))[0]
-    test_indices = np.where(np.isin(patient_info['uid'], test_uids))[0]
+class Graph_test(torch.nn.Module):
+    def __init__(self, _debug = False, groups = [21, 6, 17, 14, 20], mode =  'random'):
+        super().__init__()
+        self.mode = mode
+        self.debug = _debug
+        self.total = sum(groups)
+        self.groups = groups
+        self.ratio = 0.2
+        # self.Radio_autoencoder = nn.Sequential(
+        #     nn.LayerNorm(1781),
+        #     nn.Linear(1781, 20),
+        #     nn.LeakyReLU(),
+        #     nn.Dropout(p=0.2)
+        # )
+        self.vision_autoencoder = nn.Sequential(
+            nn.LayerNorm(384),
+            nn.Linear(384, 20),
+            nn.LeakyReLU(),
+            nn.Dropout(p=0.2)
+        )
+        self.conv1 = TransformerConv(1, 1, heads=4, concat=True)
+        self.conv2 = TransformerConv(4, 1, heads=1, concat=True)
+        # self.conv1 = GraphConv(1, 4)
+        # self.conv2 = GraphConv(4, 1)
+        # self.conv1 = GCNConv(1, 4)
+        # self.conv2 = GCNConv(4, 1)
+        # self.conv1 = DenseGraphConv(1, 4)
+        # self.conv2 = DenseGraphConv(4, 1)
 
-    # _, g = graph_bulider(patient_info)
-    x, edge_index, edge_attr = adj_matrix(patient_info, mode = experiment_settings['Mode'])
+        self.classifier = nn.Sequential(
+            nn.LayerNorm(self.total),
+            nn.Linear(self.total, 2)
+        )
+        # Initialize weights for layers
+        self.apply(self._initialize_weights)
+        
+    def _initialize_weights(self, m):
+        if isinstance(m, nn.Linear):
+            print(f'layer {m} initialized')
+            nn.init.xavier_uniform_(m.weight)  # Xavier initialization
+            if m.bias is not None:
+                nn.init.zeros_(m.bias)
+        
+    def forward(self, cli_data, radio_data):
+        radio_hidden = self.vision_autoencoder(radio_data)
+        temp_features = torch.cat([cli_data, radio_hidden], dim=1)
+        edges = []
 
-    data = Data(x=x, edge_index=edge_index, edge_attr=edge_attr)
+        x = temp_features.view(-1, 1)
 
-    # train_dataset = torch.utils.data.Subset(dataset, train_index)
-    # test_dataset = torch.utils.data.Subset(dataset, test_index)
+        if self.mode == 'fully_connected':
+            for i in range(self.total):
+                for j in range(self.total):
+                    if i != j:
+                        edges.append((i, j))
 
-    # trainDataLoader = torch.utils.data.DataLoader(train_dataset, batch_size=experiment_settings['Batch'], shuffle=True,
-    #                                               num_workers=4, drop_last=True)
-    # testDataLoader = torch.utils.data.DataLoader(test_dataset, batch_size=experiment_settings['Batch'], shuffle=False,
-    #                                              num_workers=4)
-    '''
-    Training logs and monitors
-    '''
-    target_dir = Path('./logs/')
-    target_dir.mkdir(exist_ok=True)
-    target_dir = target_dir.joinpath('classification')
-    target_dir.mkdir(exist_ok=True)
-    target_dir = target_dir.joinpath(experiment_settings['Name'])
-    target_dir.mkdir(exist_ok=True)
-    current_time = str(datetime.now().strftime('%Y-%m-%d_%H-%M'))
-    target_dir = target_dir.joinpath('fold_{}'.format(fold) + current_time)
-    target_dir.mkdir(exist_ok=True)
-    checkpoints_dir = target_dir.joinpath('checkpoints')
-    checkpoints_dir.mkdir(exist_ok=True)
-    log_dir = target_dir.joinpath('logs')
-    log_dir.mkdir(exist_ok=True)
+        elif self.mode == 'random':
+            # full connect in one group
+            offset = 0
+            for num in self.groups:
+                for i in range(num):
+                    for j in range(num):
+                        if i + offset != j + offset:
+                            edges.append((i + offset, j + offset))
+                offset += num
+            # random connect between groups
+            offset = 0
+            count = 0
+            for i in range(5):
+                offset_2 = self.groups[i]
+                for j in range(i + 1, 5):
+                    # print(i, j)
+                    for _i in range(self.groups[i]):
+                        for _j in range(self.groups[j]):
+                            # print(_i, _j)
+                            if _i + offset != _j + offset_2 and random.random() < self.ratio:
+                                edges.append((_i + offset, _j + offset_2))
+                                # print(f"({_i + offset}, {_j + offset_2})")
+                                count += 1
+                                
+                    offset_2 += self.groups[j]
+                offset += self.groups[i]
+            # print(self.ratio)
+            # print(count)
+            # print(len(edges))
+            # exit(0)
+        edge_index = torch.tensor(edges, dtype=torch.long).t().contiguous().to(device)
 
-    observer = Runtime_Observer(log_dir=log_dir, checkpoints_dir= checkpoints_dir, device=device, name=experiment_settings['Name'], seed=seed)
-    observer.log(f'[DEBUG]Observer init successfully, program start @{current_time}\n')
-    '''
-    Model load
-    '''
-    _model = experiment_settings['Model']
-    if 'Input_args' in experiment_settings:
-        input_args = experiment_settings['Input_args']
-        model = _model(**input_args)
-    else:
-        model = _model()
-    # 如果有多个GPU可用，使用DataParallel来并行化模型
-    if torch.cuda.device_count() > 1 and data_parallel == 1:
-        observer.log("Using" + str(torch.cuda.device_count()) + "GPUs for training.\n")
-        model = torch.nn.DataParallel(model)
-    observer.log(f'Use model : {str(experiment_settings)}\n')
-    num_params = 0
-    for p in model.parameters():
-        if p.requires_grad:
-            num_params += p.numel()
-    observer.log("\n===============================================\n")
-    observer.log("model parameters: " + str(num_params))
-    observer.log("\n===============================================\n")
+        x = self.conv1(x, edge_index)
+        x = F.leaky_relu(x)
+        x = self.conv2(x, edge_index)
+        x = F.leaky_relu(x)
+        x = x.permute(1, 0)
+        x = self.classifier(x)
+        return x
 
-    '''
-    Hyper parameter settings
-    '''
-    optimizer = experiment_settings['Optimizer'](model.parameters(), experiment_settings['Lr'])
-    # if experiment_settings['w1'] is not None:
-    if 'w1' in experiment_settings:
-        criterion = experiment_settings['Loss'](w1 = experiment_settings['w1'], w2 = experiment_settings['w2'], w3 = experiment_settings['w3'])
-    else:
-        criterion = experiment_settings['Loss']()
+    def _print(self, x):
+        if self.debug:
+            print(x)
 
-    print("prepare completed! launch training!\U0001F680")
-    observer.log(f"\nInitial memory allocated: {torch.cuda.memory_allocated(device='cuda') / (1024 ** 2):.2f} MB\n")
 
-    # launch
-    start_time = time.time()
-    _run = experiment_settings['Run']
-    _run(observer, patient_info, data, train_indices, test_indices, experiment_settings['Epoch'], model, device, optimizer, criterion)
-    end_time = time.time()
-    # observer.log(f"\nRunning time: {end_time - start_time:.2f} / 60 minutes\n")
+device = 'cuda:7'
 
 if __name__ == "__main__":
-    # Adding necessary input arguments
-    parser = argparse.ArgumentParser(description="add arguments to test")
-    parser.add_argument("--model", default='ROI_vision_only', type=str, help="model")
-    parser.add_argument("--seed", default=42, type=int, help="seed given by LinkStart.py on cross Val")
-    parser.add_argument("--device", default='cuda', type=str)
-    parser.add_argument("--fold", default=0, type=int, help="0~4")
-    parser.add_argument("--data_parallel", default=0, type=int, help="test on server")
-    args = parser.parse_args()
+    seed = 666
+    
+    kf = KFold(n_splits=5, shuffle=True, random_state=seed)
+    torch.manual_seed(seed)
+    for i in range(1):
+        i = 4 
+        dataset = Liver_dataset(fold=i)
+        train_index, test_index = [[t1, t2] for t1, t2 in kf.split(dataset)][i]
+        if not os.path.exists(f"debug/test{i}"):
+            os.makedirs(f"debug/test{i}")
+        observer = Runtime_Observer(log_dir=f"debug/test{i}", device=device, name="debug", seed=seed)
+    
+        train_dataset = torch.utils.data.Subset(dataset, train_index)
+        test_dataset = torch.utils.data.Subset(dataset, test_index)
+ 
+        train_loader = torch.utils.data.DataLoader(train_dataset, batch_size=1, shuffle=False, num_workers=4, drop_last=False) 
+        test_loader = torch.utils.data.DataLoader(test_dataset, batch_size=1, shuffle=False, num_workers=4)
 
-    print(args)
-    # for i in range(5):
-    #     prepare_to_train(model_index=args.model, seed=args.seed, device=args.device, fold= i , data_parallel=args.data_parallel)
+        model = Graph_test(_debug=True, mode= 'random')
+        optimizer = Adam(model.parameters(), weight_decay=1e-4)
+        criterion = CrossEntropyLoss()
+        num_params = 0
+        for p in model.parameters():
+            if p.requires_grad:
+                num_params += p.numel()
+        observer.log("\n===============================================\n")
+        observer.log("model parameters: " + str(num_params))
+        observer.log("\n===============================================\n")
+    
+        epochs = 200
+        model = model.to(device)
+        observer.log("start training\n")
+        start_time = time.time()
+        for epoch in range(epochs):
+            print(f"Epoch: {epoch + 1}/{epochs}")
+
+            observer.reset()
+            model.train()
+            train_bar = tqdm(train_loader, leave=True, file=sys.stdout)
+            running_loss = test_loss = 0.0
+            for i, (info1, info2, label) in enumerate(train_bar):
+                optimizer.zero_grad()
+                info1, info2, label = info1.to(device), info2.to(device), label.to(device)
+                outputs = model(info1, info2)
+                loss = criterion(outputs, label)
+                loss.backward()
+                optimizer.step()
+                # for name, param in model.named_parameters():
+                #     print(name, param.grad)
+                # exit()
+                running_loss += loss.item() * info1.size(0)
+            train_loss = running_loss / len(train_loader.dataset)
+            observer.log(f"Loss: {train_loss:.4f}\n")
+
+            with torch.no_grad():
+                model.eval()
+                test_bar = tqdm(test_loader, leave=True, file=sys.stdout)
+
+                for i, (info1, info2, label) in enumerate(test_bar):
+                    info1, info2, label = info1.to(device), info2.to(device), label.to(device)
+                    outputs = model(info1, info2)
+                    loss = criterion(outputs, label)
+                    probabilities = torch.softmax(outputs, dim=1)
+                    _, predictions = torch.max(outputs, dim=1)
+                    confidence_scores = probabilities[range(len(predictions)), 1]
+                    observer.update(predictions, label, confidence_scores)
+                    test_loss += loss.item() * info1.size(0)
+                test_loss = test_loss / len(train_loader.dataset)
+                observer.log(f"Test Loss: {test_loss:.4f}\n")
+
+            observer.record_loss(epoch, train_loss, test_loss)
+            if observer.excute(epoch, model):
+                print("Early stopping")
+                break
+        end_time = time.time()
+        observer.log(f"\nRunning time: {end_time - start_time:.2f} seconds\n")
+        observer.finish()
         
-    prepare_to_train(model_index=args.model, seed=args.seed, device=args.device, fold=args.fold, data_parallel=args.data_parallel)
+
+
